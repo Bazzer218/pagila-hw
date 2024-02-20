366acf65b996264c0e1ba6e220301ee06afe7991
Baron Zeng <bzeng24@cmc.edu>
2024-02-19 20:11:06 -0800

    third

--- sql/16.sql
+++ sql/16.sql
@@ -3,3 +3,10 @@
  * where the profit is the total amount that customer have payer for the film.
  * Use tables payment, rental, inventory, and film. 
  */
+select f.title, sum(p.amount) as profit
+from film as f
+join inventory as i on f.film_id = i.film_id
+join rental as r on i.inventory_id = r.inventory_id
+join payment as p on r.rental_id = p.rental_id
+group by f.title
+order by sum(p.amount) DESC;
--- sql/17.sql
+++ sql/17.sql
@@ -4,3 +4,12 @@
  * Use tables payment, rental, customer, address, city, and country.
  * Order by country alphabetically.
  */
+select cn.country, sum(py.amount) as profit
+from country as cn
+join city as ct on cn.country_id = ct.country_id
+join address as ad on ct.city_id = ad.city_id
+join customer as cu on ad.address_id = cu.address_id
+join rental as re on cu.customer_id = re.customer_id
+join payment as py on re.rental_id = py.rental_id
+group by cn.country
+order by country;
